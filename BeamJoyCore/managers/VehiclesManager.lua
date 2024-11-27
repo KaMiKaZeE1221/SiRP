f1501458a3e410a0e909306aa92f61d1148ad32d
Willis <118511278+KaMiKaZeE1221@users.noreply.github.com>
2024-11-27 10:48:05 +1030

    Update VehiclesManager.lua
    Added the ability for vehicles to cost reputation points, per spawn/edit/reset.

--- BeamJoyCore/managers/VehiclesManager.lua
+++ BeamJoyCore/managers/VehiclesManager.lua
@@ -1,11 +1,54 @@
 local M = {
     Data = {},
+    vehiclePrices = {
+        ["atv"] = 0,
+        ["autobello"] = 5,
+        ["barstow"] = 25,
+        ["bastion"] = 35,
+        ["bluebuck"] = 25,
+        ["bolide"] = 30,
+        ["burnside"] = 25,
+        ["bx"] = 20,
+        ["citybus"] = 100,
+        ["covet"] = 25,
+        ["etk800"] = 25,
+        ["etkc"] = 20,
+        ["etki"] = 20,
+        ["fullsize"] = 69,
+        ["hopper"] = 35,
+        ["lansdale"] = 30,
+        ["legran"] = 20,
+        ["md_series"] = 250,
+        ["midsize"] = 45,
+        ["midtruck"] = 45,
+        ["miramar"] = 5,
+        ["moonhawk"] = 15,
+        ["pessima"] = 25,
+        ["pickup"] = 45,
+        ["pigeon"] = 0,
+        ["racetruck"] = 35,
+        ["roamer"] = 40,
+        ["rockbouncer"] = 25,
+        ["sbr"] = 30,
+        ["scintilla"] = 100,
+        ["sunburst"] = 45,
+        ["us_semi"] = 300,
+        ["utv"] = 5,
+        ["van"] = 40,
+        ["vivace"] = 35,
+        ["wendover"] = 20,
+        ["wigeon"] = 0,
+        ["talent"] = 35,
+        ["charger"] = 35,
+        ["estr34"] = 35,
+    }
 }
 
 local function init()
     M.Data = BJCDao.vehicles.findAll()
 end
 
+--VEHICLE SPAWNING
 function _BJCOnVehicleSpawn(playerID, vehID, vehData)
     local s, e = vehData:find('%{')
     vehData = vehData:sub(s)
@@ -28,29 +71,34 @@ function _BJCOnVehicleSpawn(playerID, vehID, vehData)
         return 1
     end
 
-    if not BJCScenario.canSpawnVehicle(playerID, vehID, vehData) then
-        BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, "players.cannotSpawnVehicle")
+    local model = vehData.jbm or vehData.vcf.model
+    local vehiclePrice = M.vehiclePrices[model] or 10  -- Use vehiclePrices table
+	
+	if player.reputation < vehiclePrice then
+        BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, " Not enough reputation to reset the vehicle. You need ".. vehiclePrice .. " reputation points.")
         return 1
     end
 
+    player.reputation = player.reputation - vehiclePrice
+    BJCTx.player.toast(playerID, BJC_TOAST_TYPES.SUCCESS, " Vehicle spawned at the cost of " .. vehiclePrice .. " reputation points.")
+    -- Save the updated player data
+    BJCDao.players.save(player)
+
     if vehData.jbm == "unicycle" then
-        -- starting to walk
+        -- Special case for unicycle (walking)
         if group.vehicleCap == 0 then
             BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, "players.cannotSpawnVehicle")
             return 1
-        elseif not BJCConfig.Data.Freeroam.AllowUnicycle or
-            not BJCScenario.canWalk(playerID) then
+        elseif not BJCConfig.Data.Freeroam.AllowUnicycle or not BJCScenario.canWalk(playerID) then
             BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, "players.walkNotAllowed")
             return 1
         end
     else
-        -- spawning vehicle
+        -- General vehicle spawning
         if group.vehicleCap > -1 and group.vehicleCap <= tlength(player.vehicles) then
             BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, "players.cannotSpawnVehicle")
             return 1
         end
-
-        local model = vehData.jbm or vehData.vcf.model
 
         if tincludes(BJCVehicles.Data.ModelBlacklist, model, true) then
             if BJCPerm.isStaff(playerID) then
@@ -61,6 +109,7 @@ function _BJCOnVehicleSpawn(playerID, vehID, vehData)
             end
         end
 
+        -- Add the vehicle to the player's list of vehicles
         player.vehicles[vehID] = {
             vehicleID = vehID,
             vid = vehData.vid,
@@ -71,55 +120,32 @@ function _BJCOnVehicleSpawn(playerID, vehID, vehData)
         }
     end
 
-
     BJCTx.cache.invalidate(playerID, BJCCache.CACHES.USER)
     BJCTx.cache.invalidate(BJCTx.ALL_PLAYERS, BJCCache.CACHES.PLAYERS)
 end
 
-function _BJCOnVehicleEdited(playerID, vehID, vehData)
-    local s, e = vehData:find('%{')
-    vehData = vehData:sub(s)
-    vehData = JSON.parse(vehData)
-
-    if not vehData then
-        LogError(svar(BJCLang.getConsoleMessage("players.invalidVehicleData"), { playerID = playerID }))
-    end
-
-    if not BJCScenario.canEditVehicle(playerID, vehID, vehData) then
-        return 1
-    end
-
-    local model = vehData.jbm or vehData.vcf.model
-
-    if tincludes(BJCVehicles.Data.ModelBlacklist, model, true) then
-        if BJCPerm.isStaff(playerID) then
-            BJCTx.player.toast(playerID, BJC_TOAST_TYPES.WARNING, "players.blacklistedVehicle")
-        else
-            BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, "players.blacklistedVehicle")
-            return 1
-        end
-    end
+--VEHICLE RESET
+function _BJCOnVehicleReset(playerID, vehID, posRot)
+    posRot = JSON.parse(posRot)
 
     local player = BJCPlayers.Players[playerID]
-    if vehData and player then
-        local vehicle = player.vehicles[vehID]
-        if vehicle then
-            -- NO vid ON THIS EVENT
-            if vehicle.name ~= vehData.jbm then
-                vehicle.name = model
-                player.currentVehicle = vehicle.vid
+    local vehicle = player.vehicles[vehID]
+    if not vehicle then return end
 
-                BJCTx.cache.invalidate(playerID, BJCCache.CACHES.USER)
-                BJCTx.cache.invalidate(BJCTx.ALL_PLAYERS, BJCCache.CACHES.PLAYERS)
-            end
-        end
+    local vehiclePrice = M.vehiclePrices[vehicle.name] or 10
+
+    if player.reputation < vehiclePrice then
+		BJCTx.player.toast(playerID, BJC_TOAST_TYPES.ERROR, " Not enough reputation to reset the vehicle. You need ".. vehiclePrice .. " reputation points.")
+		MP.RemoveVehicle(playerID, vehID, vehData)
+		_BJCOnVehicleDeleted(playerID, vehID)
+        return 1		
     end
-end
 
-function _BJCOnVehicleReset(playerID, vehID, posRot)
-    -- NO USE FOR NOW
-    posRot = JSON.parse(posRot)
-end
+    player.reputation = player.reputation - vehiclePrice
+	BJCTx.player.toast(playerID, BJC_TOAST_TYPES.SUCCESS, " Vehicle spawned at the cost of " .. vehiclePrice .. " reputation points.")
+    -- Save the updated player data
+    BJCDao.players.save(player)
+	end
 
 function _BJCOnVehicleDeleted(playerID, vehID)
     local player = BJCPlayers.Players[playerID]
@@ -128,8 +154,7 @@ function _BJCOnVehicleDeleted(playerID, vehID)
         return
     end
 
-    local isCurrent = player.vehicles[vehID] and
-        player.vehicles[vehID].vid == player.currentVehicle
+    local isCurrent = player.vehicles[vehID] and player.vehicles[vehID].vid == player.currentVehicle
     player.vehicles[vehID] = nil
     if isCurrent then
         player.currentVehicle = nil
@@ -141,6 +166,7 @@ function _BJCOnVehicleDeleted(playerID, vehID)
 end
 
 local function initHooks()
+    MP.RegisterEvent("onPlayerJoin", "onPlayerJoin")
     MP.RegisterEvent("onVehicleSpawn", "_BJCOnVehicleSpawn")
     MP.RegisterEvent("onVehicleEdited", "_BJCOnVehicleEdited")
     MP.RegisterEvent("onVehicleDeleted", "_BJCOnVehicleDeleted")
@@ -190,6 +216,8 @@ M.getCache = getCache
 M.getCacheHash = getCacheHash
 
 M.onDriftEnded = onDriftEnded
+
+M.deleteVehicle = deleteVehicle
 
 init()
 initHooks()
